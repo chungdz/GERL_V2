9ad6d7c8d19ffaa1d537e04465376043e0389061
Yunfan Hu <1033718596@qq.com>
2021-05-13 11:40:01 +0800

    no sample

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 export GERL=/data/yunfanhu/GERL_V2
 mkdir data
 cd data
-mkdir L L/train L/dev L/test vocabs
+mkdir L L/train L/dev L/test L/examples vocabs
 cd ..
 
 
@@ -9,10 +9,10 @@ python -m src.scripts.build_train_samples
 python -m src.scripts.build_vocabs
 python -m src.scripts.build_neighbors
 python -m src.scripts.build_neighbors --type=test
-python -m src.scripts.build_train_examples
+python -m src.scripts.build_training_examples
 python -m src.scripts.build_eval_examples
 python -m src.scripts.build_test_examples
 python -m src.scripts.split_data
 python -m src.scripts.split_data -i L/examples/test_examples.tsv -o L/examples/test_examples
-CUDA_VISIBLE_DEVICES=0,1,2,3 python src.train
-CUDA_VISIBLE_DEVICES=0,1,2,3 python src.test training.validate_epoch=6  
+CUDA_VISIBLE_DEVICES=0,1,2,3 python -m src.train
+CUDA_VISIBLE_DEVICES=0,1,2,3 python -m src.test training.validate_epoch=6  
