43baf159290f62ae931d86bab8c5f36ca053de4d
Yunfan Hu <1033718596@qq.com>
2021-05-13 11:24:02 +0800

    add L

--- run.sh
+++ run.sh
@@ -1,18 +1,18 @@
 export GERL=/data/yunfanhu/GERL_V2
 mkdir data
 cd data
-mkdir train dev test vocabs
+mkdir L L/train L/dev L/test vocabs
 cd ..
 
 
-python -m src.scripts.build_train_samples.py
-python -m src.scripts.build_vocabs.py
-python -m src.scripts.build_neighbors.py
-python -m src.scripts.build_neighbors.py --type=test
-python -m src.scripts.build_train_examples.py
-python -m src.scripts.build_eval_examples.py
-python -m src.scripts.build_test_examples.py
-python -m src.scripts.split_data.py 
-python -m src.scripts.split_data.py -i L/examples/test_examples.tsv -o L/examples/test_examples
-CUDA_VISIBLE_DEVICES=0,1,2,3 python src.train.py 
-CUDA_VISIBLE_DEVICES=0,1,2,3 python src.test.py training.validate_epoch=6  
+python -m src.scripts.build_train_samples
+python -m src.scripts.build_vocabs
+python -m src.scripts.build_neighbors
+python -m src.scripts.build_neighbors --type=test
+python -m src.scripts.build_train_examples
+python -m src.scripts.build_eval_examples
+python -m src.scripts.build_test_examples
+python -m src.scripts.split_data
+python -m src.scripts.split_data -i L/examples/test_examples.tsv -o L/examples/test_examples
+CUDA_VISIBLE_DEVICES=0,1,2,3 python src.train
+CUDA_VISIBLE_DEVICES=0,1,2,3 python src.test training.validate_epoch=6  
--- src/scripts/build_train_samples.py
+++ src/scripts/build_train_samples.py
@@ -51,7 +51,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     # Path options.
-    parser.add_argument("--fsize", default="S", type=str,
+    parser.add_argument("--fsize", default="L", type=str,
                         help="Corpus size")
     parser.add_argument("--neg_count", default=4, type=int,
                         help="Max neg samples according to one pos sample.")
